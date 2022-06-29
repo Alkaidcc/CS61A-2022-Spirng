2239d97c73b809944e97d46b0089b1c25a1e9840
Alkaidcc <932214806@qq.com>
2022-06-29 11:05:53 +0800

    complete lab01

---
+++ labs/lab01/parsons_probs/k_occurrence.py
@@ -0,0 +1,19 @@
+def k_occurrence(k, num):
+    """
+    >>> k_occurrence(5, 10)  # .Case 1
+    0
+    >>> k_occurrence(5, 5115)  # .Case 2
+    2
+    >>> k_occurrence(0, 100)  # .Case 3
+    2
+    >>> k_occurrence(0, 0)  # .Case 4
+    0
+    """
+    if k == num == 0:
+        return 0
+    cnt = 0
+    while num > 0:
+        if num % 10 == k:
+            cnt += 1
+        num //= 10
+    return cnt
