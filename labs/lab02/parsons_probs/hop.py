8f73d1270a3942cea92d0e471669c4a3e22646c5
Alkaidcc <932214806@qq.com>
2022-07-02 11:29:57 +0800

    complete lab02

---
+++ labs/lab02/parsons_probs/hop.py
@@ -0,0 +1,16 @@
+def hop():
+    """
+    Calling hop returns a curried version of the function f(x, y) = y.
+    >>> hop()(3)(2) # .Case 1
+    2
+    >>> hop()(3)(7) # .Case 2
+    7
+    >>> hop()(4)(7) # .Case 3
+    7
+    """
+    def h1(x):
+        def h2(y):
+            return y
+        return h2
+    return h1
+
