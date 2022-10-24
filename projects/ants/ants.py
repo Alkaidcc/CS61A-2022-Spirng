765646d91713eb1387526550043a7b2505544bc3
alkaidcc <932214806@qq.com>
2022-10-24 20:38:56 +0800

    ants: problem 07

--- projects/ants/ants.py
+++ projects/ants/ants.py
@@ -293,7 +293,23 @@ class WallAnt(Ant):
 # END Problem 6
 
 # BEGIN Problem 7
-# The HungryAnt Class
+class HungryAnt(Ant):
+    name = 'Hungry'
+    food_cost = 4
+    time_to_chew = 3
+    implemented = True
+    def __init__(self, health=1):
+        """Create an Ant with a HEALTH quantity."""
+        self.chew_timer = 0
+        super().__init__(health)
+    def action(self, gamestate):
+        if self.chew_timer == 0:
+            bee = random_bee(self.place.bees)
+            if bee:
+                bee.reduce_health(bee.health)
+                self.chew_timer = self.time_to_chew
+        else:
+            self.chew_timer -= 1
 # END Problem 7
 
 
